46709028d601f2d3397fcff74634e743b01cf46b
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-09-30 23:15:17 +0300

    Updates

---
+++ precache-manifest.6250bf7f682a491f38ff9713bb79d05e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3bfc013d1da88c6736b",
+    "url": "/course-directory/static/css/main.6d36d66d.chunk.css"
+  },
+  {
+    "revision": "f3bfc013d1da88c6736b",
+    "url": "/course-directory/static/js/main.f3bfc013.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "99558c1aa59c51eafa3dca81702c7f3c",
+    "url": "/course-directory/index.html"
+  }
+];
